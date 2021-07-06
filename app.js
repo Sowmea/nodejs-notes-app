08aff6e2f0195fb6b1ff77d6d0f1fe45ce1ae168
sowmahe993@gmail.com <sowmahe993@gmail.com>
2021-07-06 14:10:37 +0800

    usage of chalk

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // const add = require('./utils');
+const chalk = require('chalk');
 
 // console.log(add(2, 3));
-
 const print = require('./notes');
 const msg = print();
 console.log(msg);
+
+const greenMessage = chalk.green.bold.bgYellowBright.inverse;
+console.log(greenMessage('Success!'));
+
+console.log(process.argv[2]);
